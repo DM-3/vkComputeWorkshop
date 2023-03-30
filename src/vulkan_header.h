323d858c60e39cfee679c9401866808e7440de23
DM-3 <97919462+DM-3@users.noreply.github.com>
2023-03-30 16:43:39 +0200

    Update vulkan_header.h

--- src/vulkan_header.h
+++ src/vulkan_header.h
@@ -1,2 +1,47 @@
-#include "vulkan_utils"
+#include "vulkan_utils.h"
+
+class VKC {
+public:
+  void startVulkan()  {}
+  void execute()      {}
+  void endVulkan()    {}
+  
+private:
+  VkInstance instance;
+  VkDevice device;
+  
+  void createInstance()     {}
+  void createDevice()       {}
+  void createQueue()        {}
+  void createCommandPool()  {}
+  
+  void getDevice()      {}
+  void getQueue()       {}
+  void getCommandPool() {}
+  
+};
+
+VKC::createInstance() {
+  VkApplicationInfo appInfo{};
+  appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+  appInfo.pApplicationName = "Hello Compute Shader";
+  appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+  appInfo.pEngineName = "No Engine";
+  appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+  appInfo.apiVersion = VK_API_VERSION_1_0;
+
+  VkInstanceCreateInfo instanceCI{};
+  instanceCI.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  instanceCI.pApplicationInfo = &appInfo;
+  instanceCI.enabledExtensionCount = 0;
+  instanceCI.ppEnabledExtensionNames = nullptr;
+  instanceCI.enabledLayerCount = 0;
+  
+  
+}
+
+
+
+
+
 
