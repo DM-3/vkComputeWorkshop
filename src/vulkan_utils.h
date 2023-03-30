bb77aed179a61e8cf657ce970abea91038bb41ca
DM-3 <97919462+DM-3@users.noreply.github.com>
2023-03-30 17:01:55 +0200

    Update vulkan_utils.h

--- src/vulkan_utils.h
+++ src/vulkan_utils.h
@@ -1,8 +1,45 @@
-#include <vulkan>
+#include <vulkan/vulkan.h>
+#include <iostream>
+#include <vector>
+#include <cstring>
 
 #define ASSERT_VULKAN
 
 
 namespace vkc {
+  
+  const std::vector<const char*> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"
+  };
+  
+  #ifdef NDEBUG
+    const bool enableValidationLayers = false;
+  #else
+    const bool enableValidationLayers = true;
+  #endif
+  
   VkResult result;
+    
+  
+  bool checkValidationLayerSupport() {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+    
+    for (const char* layerName : validationLayers) {
+      bool layerFound = false;
+
+      for (const auto& layerProperties : availableLayers) {
+          if (strcmp(layerName, layerProperties.layerName) == 0) {
+              layerFound = true;
+              break;
+          }
+      }
+      if (!layerFound) return false;
+    }
+
+    return true;
+  }
 };
