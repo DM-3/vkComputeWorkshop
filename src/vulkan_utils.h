fb9a2091571285d788882037d566141a2f9910a3
DM-3 <97919462+DM-3@users.noreply.github.com>
2023-03-30 19:59:37 +0200

    Update vulkan_utils.h

--- src/vulkan_utils.h
+++ src/vulkan_utils.h
@@ -76,6 +76,21 @@ namespace vkc {
     return shaderModule;
   }
   
+  // function
+  uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties) {
+    VkPhysicalDeviceMemoryProperties memoryProperties;
+    vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memoryProperties);
+    
+    for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
+      if ((typeFilter & (1 << i)) && (memProperties.memoryTypes[i].propertyFlags & properties) == properties) {
+        return i;
+      }
+    }
+    
+    throw std::runtime_error("failed to find suitable memory type");
+  }
+  
+  
 };
 
 
