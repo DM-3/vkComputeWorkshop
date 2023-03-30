cf1c633244b9fc4d52ea7459ec50c4323de8b2f8
DM-3 <97919462+DM-3@users.noreply.github.com>
2023-03-30 20:24:27 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include "vulkan_header.h"
+#define INPUT_SIZE 128
+#define OUTPUT_SIZE 128
+
 
 int main() {
+  VKC computation;
+  
+  computation.setInputSize(INPUT_SIZE * sizeof(float));
+  computation.setOutputSize(OUTPUT_SIZE * sizeof(float));
+  
+  computation.startVulkan();
+  
+  float arr_in[INPUT_SIZE];
+  float arr_out[OUTPUT_SIZE];
+  
+  computation.execute();
+  computation.endVulkan();
   
   return 1;
 }
