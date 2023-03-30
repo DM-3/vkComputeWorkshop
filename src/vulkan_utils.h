348a1bfa1b51fd424e13af1c72296f5cc8501ab5
DM-3 <97919462+DM-3@users.noreply.github.com>
2023-03-30 18:09:18 +0200

    Update vulkan_utils.h

--- src/vulkan_utils.h
+++ src/vulkan_utils.h
@@ -3,6 +3,7 @@
 #include <stdexcept>
 #include <vector>
 #include <cstring>
+#include <fstream>
 
 #define ASSERT_VULKAN
 
@@ -21,7 +22,7 @@ namespace vkc {
   
   VkResult result;
     
-  
+  // function
   bool checkValidationLayerSupport() {
     uint32_t layerCount;
     vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
@@ -42,6 +43,23 @@ namespace vkc {
     }
 
     return true;
+  }
+  
+  // function
+  static std::vector<char> readFile(const std::string& filename) {
+    std::ifstream file(filename, std::ios::ate | std::ios::binary);
+    if (!file.is_open()) {
+        throw std::runtime_error("failed to open file!");
+    }
+    
+    size_t fileSize = (size_t) file.tellg();
+    std::vector<char> buffer(fileSize);
+    
+    file.seekg(0);
+    file.read(buffer.data(), fileSize);
+    
+    file.close();
+    return buffer;
   }
 };
 
